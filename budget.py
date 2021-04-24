777e5d156c903392f7af6235e20cf9875587462c
gatemediang <estolog@gmail.com>
2021-04-24 17:17:56 +0100

    budget.py

--- budget.py
+++ budget.py
@@ -1,32 +1,35 @@
 database = { }
 
-class Budget():
+
+class Budget:
     def __init__(self, category, amount):
         self.category = category
         self.amount = amount
 
-    def deposit(amount, bal):
-        bal += amount
-        return bal
+    def deposit(amount, balance):
+        balance += amount
+        return balance
 
-    def withdraw(user, amount, bal):
-        bal -= amount
-        return bal
+    def check_balance(db):
+        for categ, balance in db.items():
+            print(categ, balance)
 
-    def balance(db):
-        for categ, bal in db.items():
-            print(categ, bal)
+    def withdraw(category, amount, balance):
+        balance -= amount
+        return balance
 
     def transfer(db, option1, amount, option2):
         value1 = db[option1]
-        valuue2 = db[option2]
+        value2 = db[option2]
 
         db[option1] = int(value1) - amount
-        db[option2] = int(valuue2) + amount
+        db[option2] = int(value2) + amount
         return db
 
+
+
 def init():
-    print('=== **** Welcome to the Budget App**** ===\n')
+    print('=== **** Welcome to the Home Budget App**** ===\n')
     menu()
 
 
@@ -34,42 +37,46 @@ def init():
 def menu():
     try:
 
-        user = int(input('\n=== ****What would you like to do?**** ===\nPress (1) To create a new budget\nPress (2) To deposit into a budget\nPress (3) To withdraw from a budget\nPress (4) To check your budget balance\nPress (5) To transfer money between budgets\nPress (6) To quit\n'))
+        user = int(input('\n=== ****What would you like to do?**** ===\nPress (1) To create Food Budget\nPress (2) To create Clothing Budget\nPress (3) To create Entertainment Budget\nPress (4) To check your budget balance\nPress (5) To transfer money between budgets\nPress (6) To deposit into Budget Category of choice\nPress (7) to withdraw from Budget Category of choice\nPress (8) To quit \n'))
     except:
         print('Invalid input')
         menu()
 
     if (user == 1):
-        new_budget()
+        category = 'Food'
+        amount = int(input("Enter your budget amount \nNGN."))
+        category_1 = Budget(category, amount)
+        database[category] = amount
+        print(f'Budget {category} was setup with NGN.{amount}')
+        menu()
     elif (user == 2):
-        credit()
+        category = 'Clothing'
+        amount = int(input("Enter your budget amount \nNGN."))
+        category_2 = Budget(category, amount)
+        database[category] = amount
+        print(f'Budget {category} was setup with NGN.{amount}')
+        menu()
     elif (user == 3):
-        debit()
+        category = 'Entertainment'
+        amount = int(input("Enter your budget amount \nNGN."))
+        category_3 = Budget(category, amount)
+        database[category] = amount
+        print(f'Budget {category} was setup with NGN.{amount}')
+        menu()
     elif (user == 4):
         balance()
     elif (user == 5):
         transfer()
-    elif (user == 6):
+    elif(user == 6):
+        credit()
+    elif(user == 7):
+        debit()
+    elif (user == 8):
         out()
     else:
         print('Invalid input\n')
         menu()
 
-
-def new_budget():
-    print("\n=== ***Creating a New Budget**** ===\n")
-
-    budget_title = input("Enter budget name \n")
-    try:
-        amount = int(input("Enter your budget amount \n$"))
-    except:
-        print('\nInvalid input')
-        new_budget()
-    budget = Budget(budget_title, amount)
-    database[budget_title] = amount
-    print('')
-    print(f'Budget {budget_title} was setup with ${amount}')
-    menu()
 
 def debit():
     print("=== ****Withdraw from a created budget**** ===\n")
@@ -80,26 +87,26 @@ def debit():
 
     pick = int(input('\nPress (1) To continue with your debit transaction\nPress (2) To stop debit transaction\n'))
     if (pick == 1):
-        user = input("\n**** Select one of budget(s) aforementioned ****\n")
-        if user in database:
-            print('Note: You can not withdraw all your budget, at least $1 must remain.')
-            amt = int(input("Enter amount \n$"))
-            if amt < database[user]:
-                balance = int(database[user])
-                new_balance = Budget.withdraw(user, amt, balance)
-                database[user] = new_balance
-                print(f"${amt} has been debited from Budget-{user}\nBudget amount remaining ${new_balance}")
+        category = input("\n**** Select one of the budget categories to withdraw from ****\n")
+        if category in database:
+            print('Note: You can not withdraw all your budget, at least NGN1 must remain.')
+            amount = int(input("Enter amount \nNGN"))
+            if amount < database[category]:
+                balance = int(database[category])
+                new_balance = Budget.withdraw(category, amount, balance)
+                database[category] = new_balance
+                print(f"NGN.{amount} has been debited from Budget-{category}\nBudget amount remaining NGN.{new_balance}")
                 menu()
 
             else:
-                pick = int(input(f'\nBudget {user} is insufficient of the ${amt} required\nThe actual balance {database[user]}\n\nPress (1) To deposit to the budget\nPress (2) To choose the right budget\n'))
+                pick = int(input(f'\nBudget {category} is insufficient of the NGN.{amount} required\nThe actual balance {database[category]}\n\nPress (1) To deposit to the budget\nPress (2) To choose the right budget\n'))
                 if (pick == 1):
-                    amt = int(input("Enter amount \n$"))
-                    balance = int(database[user])
-                    new_balance = Budget.deposit(amt, balance)
-                    database[user] = new_balance
+                    amount = int(input("Enter amount \n$"))
+                    balance = int(database[category])
+                    new_balance = Budget.deposit(amount, balance)
+                    database[category] = new_balance
                     print('')
-                    print(f"Budgets {user} has been credited with ${amt}\n")
+                    print(f"Budgets {category} has been credited with NGN.{amount}\n")
                     debit()
 
                 elif (pick == 2):
@@ -108,23 +115,7 @@ def debit():
                     print('Invalid option\n')
                     debit()
         else:
-          pick = int(input(f'\n****  Budget {user} does not exist! ****\nPress (1) To create a new budget\nPress (2) To choose the right budget\nPress (3) To move to the menu\n'))
-          if (pick == 1):
-              new_budget()
-          elif (pick == 2):
-              debit()
-          elif (pick == 3):
-              print('')
-              menu()
-          else:
-              print('Invalid option\n')
-              debit()
-    elif (pick == 2):
-        print('\nYou have terminated the debit transaction ')
-        menu()
-    else:
-        print('\nInvalid option')
-        debit()
+          pick = int(input(f'\n****  Budget {category} does not exist! ****\n'))
 
 
 
@@ -136,39 +127,23 @@ def credit():
 
     pick = int(input('\nPress (1) To continue with your deposit transaction\nPress (2) To stop deposit transaction\n'))
     if (pick == 1):
-        user = input("Select a budget \n")
-        if user in database:
-            amt = int(input("Enter amount \n$"))
-            balance = int(database[user])
-            new_balance = Budget.deposit(amt, balance)
-            database[user] = new_balance
-            print(f'\nBudget {user} is credited with ${amt}\nTotal Budget amount is now ${new_balance}')
+        category = input("Select a budget \n")
+        if category in database:
+            amount = int(input("Enter amount \nNGN."))
+            balance = int(database[category])
+            new_balance = Budget.deposit(amount, balance)
+            database[category] = new_balance
+            print(f'\nBudget {category} is credited with NGN.{amount}\nTotal Budget amount is now NGN.{new_balance}')
             menu()
 
         else:
             print('')
-            pick = int(input(f'Budget {user} does not exist!\nPress (1) To create a new budget\nPress (2) To choose the right budget\nPress (3) To move to the menu\n'))
-            if (pick == 1):
-                new_budget()
-            elif (pick == 2):
-                credit()
-            elif (pick == 3):
-                menu()
-            else:
-                print('Invalid option\n')
-                credit()
-
-    elif (pick == 2):
-        print('\nYou terminated the deposit transaction')
-        menu()
-    else:
-        print('\nInvalid option')
-        deposit()
+            pick = int(input(f'Budget {category} does not exist!\n'))
 
 
 def balance():
     print("*** Getting Your Budget Balance***\n")
-    check_bal = Budget.balance(database)
+    check_bal = Budget.check_balance(database)
     if (check_bal == None):
         print('')
         menu()
@@ -182,16 +157,16 @@ def transfer():
         print(key)
         print('')
     print("**** Transfer Operations ****")
-    print('Note: You can not withdraw all your budget, at least $1 must remain.\n')
-    from_budget = input("Enter the buget you are transfering from \n")
+    print('Note: You can not withdraw all your budget, at least NGN.1 must remain.\n')
+    from_budget = input("Enter the budget category you are transferring from \n")
     if from_budget in database:
-        from_amount = int(input("Enter amount you want to transfer \n$"))
+        from_amount = int(input("Enter amount you want to transfer \nNGN."))
         if from_amount < database[from_budget]:
             to_budget = input("Enter destination of funds \n")
             if to_budget in database:
                 db = Budget.transfer(database, from_budget, from_amount, to_budget)
                 print("")
-                print(f"You transfered ${from_amount} from {from_budget} to {to_budget} ")
+                print(f"You transferred NGN.{from_amount} from {from_budget} to {to_budget} ")
                 for key, value in db.items():
                     print(key, value)
                 menu()
@@ -199,7 +174,7 @@ def transfer():
                 print(f'\n{from_budget} Budget does not exist, please choose from the valid budget below\n')
                 transfer()
         else:
-            print(f'You do not have such amount-${from_amount} in {from_budget} budget')
+            print(f'You do not have such amount-NGN.{from_amount} in {from_budget} budget')
             transfer()
     else:
         print(f'Budget {from_budget} does not exist\n')
@@ -221,7 +196,5 @@ def out():
     else:
         print('Invalid input\n')
         out()
-
-
 
 init()
